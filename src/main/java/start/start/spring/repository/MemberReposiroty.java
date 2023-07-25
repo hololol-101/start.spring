df704763f0d31cf3f089343eec74b59020e192eb
hololol-101 <shrkddms1010@naver.com>
2023-07-25 23:42:23 +0900

    create member domain, repository

---
+++ src/main/java/start/start/spring/repository/MemberReposiroty.java
@@ -0,0 +1,14 @@
+package start.start.spring.repository;
+
+import start.start.spring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberReposiroty {
+    public Member save(Member member);
+    //Optional 사용 이유: find 했을 때 값이 널인경우 Optional로 감싸서 넘겨줄 수 있음
+    public Optional<Member> findById(Long id);
+    public Optional<Member> findByName(String name);
+    public List<Member> findAll();
+}
