a9d75a13b019f5cbe734209817dd10b497a52619
hololol-101 <shrkddms1010@naver.com>
2023-07-26 23:07:56 +0900

    회원 목록 웹 페이지 생성 및 기능 구현

--- src/main/java/start/start/spring/controller/MemberController.java
+++ src/main/java/start/start/spring/controller/MemberController.java
@@ -2,10 +2,13 @@ package start.start.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import start.start.spring.domain.Member;
 import start.start.spring.service.MemberService;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -22,7 +25,7 @@ public class MemberController {
         return "/members/createMemberForm";
     }
 
-    @PostMapping("members/new")
+    @PostMapping("/members/new")
     public String create(MemberForm memberForm){
         Member member = new Member();
         member.setName(memberForm.getName());
@@ -30,6 +33,13 @@ public class MemberController {
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 
 }
