24a2fb93b1ce4fdc366ef5888cf4f4829481460f
hololol-101 <shrkddms1010@naver.com>
2023-07-28 23:05:17 +0900

    jdbc로 데이터 crud

--- src/main/java/start/start/spring/SpringConfig.java
+++ src/main/java/start/start/spring/SpringConfig.java
@@ -1,13 +1,24 @@
 package start.start.spring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import start.start.spring.repository.JdbcMemberRepository;
 import start.start.spring.repository.MemberRepository;
 import start.start.spring.repository.MemoryMemberRepository;
 import start.start.spring.service.MemberService;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,6 +27,9 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return  new JdbcMemberRepository(dataSource);
     }
+
 }
