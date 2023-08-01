289aadb5b8623ccce380cdd6181107af1f6ac690
hololol-101 <shrkddms1010@naver.com>
2023-08-02 07:15:55 +0900

    Java만 사용하여 회원 도메인 사용과 테스트

---
+++ src/main/java/start/start/spring/basic/core/member/MemberApp.java
@@ -0,0 +1,15 @@
+package start.start.spring.basic.core.member;
+
+public class MemberApp {
+
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1l, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + member.getName());
+        System.out.println("find Member = "+ findMember.getName());
+
+    }
+}
